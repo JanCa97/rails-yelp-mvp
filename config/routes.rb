c2c5db7bbadd67603f323e22efb5c676ae49dec4
jan <jancarlosdiaz26@gmail.com>
2022-09-14 18:45:31 -0500

    se realizo el ejercicio

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,8 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  # root "articles#index" except: %i[edit]
+  resources :restaurants, only: %i[index new create show destroy update] do
+    post :create_review
+  end
 end
